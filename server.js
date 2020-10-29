0adc1d20d97c55d068a3f361f7f836b1fc82fb7b
Hieu <hthnguyen@myseneca.ca>
2020-10-29 18:43:29 -0400

    make it better

--- server.js
+++ server.js
@@ -20,15 +20,14 @@ console.log("Express HTTP server listening on: " + HTTP_PORT);
 app.use(express.static(path.join(__dirname, 'public')));
 
 //set up route to my default page
+app.get('/', (req, res) => res.render('public/index'))
 app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/public/html/index.html"));
-    res.sendFile(path.join(__dirname,"/public/css/main.css"));
-    res.sendFile(path.join(__dirname,"/public/css/signUp-signIn.css"));
+    res.sendFile(path.join(__dirname,"public/index.html"));
 });
 
 //set up route to my registration page
 app.get("/registration",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/public/html/registration.html"))
+    res.sendFile(path.join(__dirname,"public/registration.html"))
 });
 
 app.use((req,res)=>{
