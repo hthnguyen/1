6a611c678a8c3d02d88501b884880e34c9f143ac
Hieu <hthnguyen@myseneca.ca>
2020-10-30 02:59:43 -0400

    make it better

--- server.js
+++ server.js
@@ -4,33 +4,8 @@ app = express();
 
 
 
-// app.set('port', 8080);
-// app.use(express.static(path.join(__dirname, '../public')));
-// var server = app.listen(app.get('port'), function(){
-//     console.log("The server is running on http://localhost:" + app.get('port'));    
-// })
-
-const HTTP_PORT = process.env.HTTP_PORT || 8080;
-
-//welcome message
-function onHTTPSTART(){
-console.log("Express HTTP server listening on: " + HTTP_PORT);
-}
-
+app.set('port', 8080);
 app.use(express.static(path.join(__dirname, '../WEB322')));
-
-//set up route to my default page
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"index.html"));
+var server = app.listen(app.get('port'), function(){
+    console.log("The server is running on http://localhost:" + app.get('port'));    
 });
-
-//set up route to my registration page
-app.get("/registration",(req,res)=>{
-    res.sendFile(path.join(__dirname,"registration.html"))
-});
-
-app.use((req,res)=>{
-    res.status(404).send("Page does not exist.");
-});
-
-app.listen(HTTP_PORT,onHTTPSTART);
