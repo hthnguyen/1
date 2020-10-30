5655cd3bdbc28564a98e71ba7b15bf3a5f8179ff
Hieu <hthnguyen@myseneca.ca>
2020-10-30 03:06:00 -0400

    Commit

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+
 var express = require("express");
 path = require("path");
-app = express();
+var app = express();
 
-
-
-app.set('port', 8080);
 app.use(express.static(path.join(__dirname, '../WEB322')));
-var server = app.listen(app.get('port'), function(){
-    console.log("The server is running on http://localhost:" + app.get('port'));    
+var HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"index.html"));
 });
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT, onHttpStart);
